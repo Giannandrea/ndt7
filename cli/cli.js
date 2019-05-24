e2eaba3719ddab8a729c5142b823e23462a650fd
mauro giannandrea <mgiannandrea@domotz.com>
2019-05-24 12:03:03 +0200

    upload function (working on node > 4)

--- cli/cli.js
+++ cli/cli.js
@@ -2,9 +2,8 @@
 
 var Ndt7 = require('../index.js')
 var clc = require("cli-color");
-var speed = 0
 var output_string = ""
-var speed_test_banner = "NDT7 client"
+var NDT7_BANNER = "NDT7 client"
 
 function on_open(value) {
     console.log("Opening connection")
@@ -21,39 +20,54 @@ function on_selectedServer(value) {
 }
 
 function on_clientMeasurement(value) {
-    speed = value.toFixed(3)
+    speed = value["download_speed"].toFixed(3)
+}
+
+function on_clientMeasurement_upload(value) {
+   var complete_string = output_string + ("Upload speed: "+value["upload_speed"].toFixed(3)+" Mbit/s\r\n");
+   process.stdout.write(clc.erase.screen);
+   process.stdout.write(clc.reset);
+   clc.move.top;
+   var banner = clc.xterm(61).bgXterm(232).bold;
+   console.log(banner(NDT7_BANNER));
+   var msg = clc.xterm(178).bgXterm(0);
+   console.log(msg(complete_string));
+   clc.move.top;
 }
 
 function on_serverMeasurement(value) {
-    output_string = "Download speed: "+speed+" Mbit/s\r\n"
+   output_string = "Download speed: "+speed+" Mbit/s\r\n"
     output_string += "Max bandwidth: "+value["max_bandwidth"].toFixed(3)+" Mbit/s\r\n";
     output_string += "Min RTT: "+value["min_rtt"]+" ms\r\n";
     output_string += "Smoothed RTT: "+value["smoothed_rtt"]+" ms\r\n";
     output_string += "RTT variance: "+value["rtt_var"]+" ms\r\n";
+    output_string += "RTT variance: "+value["rtt_var"]+" ms\r\n";
+    var complete_string = output_string + ("Upload speed: N/A Mbit/s\r\n");
     process.stdout.write(clc.erase.screen);
     process.stdout.write(clc.reset);
     clc.move.top;
     var banner = clc.xterm(61).bgXterm(232).bold;
-    console.log(banner(speed_test_banner));
+    console.log(banner(NDT7_BANNER));
     var msg = clc.xterm(178).bgXterm(0);
-    console.log(msg(output_string));
+    console.log(msg(complete_string));
     clc.move.top;
 }
 
 function on_finish(value) {
     process.stdout.write(clc.reset);
     var banner = clc.xterm(61).bgXterm(232).bold;
-    console.log(banner(speed_test_banner));
+    console.log(banner(NDT7_BANNER));
     var msg = clc.green.bgXterm(0);
+    output_string += "Upload speed: "+value["upload_speed"].toFixed(3)+" Mbit/s\r\n"
     console.log(msg(output_string));
     console.log(msg("TEST FINISHED!!!!"));
 }
 
 var ndt7 = new Ndt7()
-ndt7.on("open", on_open)
 ndt7.on("error", on_error)
 ndt7.on("selectedServer", on_selectedServer)
 ndt7.on("serverMeasurement",on_serverMeasurement)
 ndt7.on("clientMeasurement", on_clientMeasurement)
-ndt7.on("close", on_finish)
+ndt7.on("clientMeasurementUpload", on_clientMeasurement_upload)
+ndt7.on("finished", on_finish)
 ndt7.start()
--- index.js
+++ index.js
@@ -8,12 +8,21 @@ function Ndt7() {
     max_bandwidth: 0,
     min_rtt: 0,
     smoothed_rtt: 0,
-    rtt_var: 0
+    rtt_var: 0,
+    upload_speed: 0
+  }
+
+  var test_progress = {
+    download_test: true,
+    upload_test: true
   }
 
   var custom_callbacks = {
     serverMeasurement: null,
-    clientMeasurement: null
+    clientMeasurement: null,
+    clientMeasurementUpload: null,
+    open: null,
+    finished: null
   }
 
   function init_callback(value, index, array) {
@@ -25,7 +34,10 @@ function Ndt7() {
         clnt.on(libndt7.libndt7().events.clientMeasurement, on_clientMeasurement)
         break;
       case "close":
-        clnt.on(libndt7.libndt7().events.close, on_finish)
+        clnt.on(libndt7.libndt7().events.close, on_close)
+        break;
+      case "open":
+        clnt.on(libndt7.libndt7().events.open, on_open)
         break;
       default:
         clnt.on(libndt7.libndt7().events[value], on_handler)
@@ -47,13 +59,36 @@ function Ndt7() {
   }
 
   function on_clientMeasurement(value) {
-    var download_speed = 8 * value["app_info"]["num_bytes"] / value["elapsed"] / 1000 / 1000
-    if (custom_callbacks.clientMeasurement)
-      eval(custom_callbacks.clientMeasurement)(download_speed)
-    return download_speed
+    var speed = 8 * value["app_info"]["num_bytes"] / value["elapsed"] / 1000 / 1000
+    if (custom_callbacks.clientMeasurement && test_progress.download_test) {
+      results["download_speed"] = speed
+      eval(custom_callbacks.clientMeasurement)(results)
+      return
+    }
+    if (custom_callbacks.clientMeasurementUpload && test_progress.upload_test) {
+      results["upload_speed"] = speed
+      eval(custom_callbacks.clientMeasurementUpload)(results)
+      return
+    }
+    return speed
   }
 
-  function on_finish(value) {
+  function on_open(value) {
+    if (custom_callbacks.oepn)
+      eval(custom_callbacks.open)(value)
+    return value
+  }
+
+  function on_close(value) {
+    if (test_progress.download_test) {
+      test_progress.download_test = false;
+      clnt.startUpload();
+      return
+    } else {
+      test_progress.upload_test = false
+      if (custom_callbacks.finished)
+        eval(custom_callbacks.finished)(results)
+    }
     return results;
   }
 
@@ -67,11 +102,8 @@ function Ndt7() {
   }
 
   Ndt7.prototype.on = function (key, callback) {
-    if (key == "serverMeasurement" || key == "clientMeasurement") {
       custom_callbacks[key] = callback
       return
-    }
-    clnt.on(libndt7.libndt7().events[key], callback)
   }
 }
 module.exports = Ndt7;
